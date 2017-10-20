8abaf7333e4e5e386a34237491cf298487048fce
berny <berny_tito@hotmail.com>
2017-10-20 08:24:25 +0200

    COntador funcionando y creado para el movil

--- Contador/app/src/main/java/com/example/infot/contador/MainActivity.java
+++ Contador/app/src/main/java/com/example/infot/contador/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.infot.contador;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //variales
+
+    public int contador;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        contador=0;
+
+    }
+
+    public void sumacontador(View vista){
+        contador++;
+        mostrarresultado();
+    }
+    public void restacontador(View vista){
+        contador --;
+        mostrarresultado();
+
+    }
+    public void resetcontador(View vista){
+        contador=0;
+        mostrarresultado();
+
+    }
+    public void mostrarresultado(){
+        TextView resultado = (TextView)findViewById(R.id.editText);
+        resultado.setText("Contador : " + contador);
     }
 }
