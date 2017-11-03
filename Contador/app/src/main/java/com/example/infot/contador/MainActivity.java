d821e9f443464c873f87ab027accd6290c4bc7c5
berny <berny_tito@hotmail.com>
2017-11-03 03:39:02 +0100

    Checker listo

--- Contador/app/src/main/java/com/example/infot/contador/MainActivity.java
+++ Contador/app/src/main/java/com/example/infot/contador/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.infot.contador;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
+
+import static java.lang.Integer.parseInt;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,18 +28,41 @@ public class MainActivity extends AppCompatActivity {
         contador++;
         mostrarresultado();
     }
-    public void restacontador(View vista){
-        contador --;
-        mostrarresultado();
 
+    public void restacontador(View vista){
+
+        CheckBox negatiu = (CheckBox)findViewById(R.id.checkBoxNegativos);
+        TextView numinicial = (TextView)findViewById(R.id.textInici);
+
+        if (numinicial.getText() == ""){
+
+        }
+        else {
+
+            contador = numinicial.isNum;
+        }
+
+        if (negatiu.isChecked()){
+
+            contador--;
+            if (contador < 0){
+                contador = 0;
+            }
+        }
+        else {
+            contador--;
+        }
+
+        mostrarresultado();
     }
+
     public void resetcontador(View vista){
         contador=0;
         mostrarresultado();
-
     }
+
     public void mostrarresultado(){
         TextView resultado = (TextView)findViewById(R.id.editText);  //Creamos una nueva vista y con findViewId buscamo la Vista
-        resultado.setText("Contador : " + contador);  
+        resultado.setText("" + contador);
     }
 }
